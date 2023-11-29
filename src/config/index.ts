354409e09879b5605e9c0fb2c8931afa900af2cf
Kartikey Khanna <kartikeykhanna20@gmail.com>
2023-11-30 02:04:48 +0530

    TRPC & payload config, Configured TRPC, payload cms, stripe webhook, stripe payment handling, collections, auth & payment routers and server config

---
+++ src/config/index.ts
@@ -0,0 +1,65 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor picks",
+        href: `/products?category=ui_kits`,
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "/products?category=ui_kits&sort=desc",
+        imageSrc: "/nav/ui-kits/blue.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "/products?category=ui_kits",
+        imageSrc: "/nav/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favorite Icon Picks",
+        href: `/products?category=icons`,
+        imageSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "/products?category=icons&sort=desc",
+        imageSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Bestselling Icons",
+        href: "/products?category=icons",
+        imageSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+  {
+    label: "Wallpapers",
+    value: "wallpapers" as const,
+    featured: [
+      {
+        name: "Favorite Wallpaper Picks",
+        href: `/products?category=wallpapers`,
+        imageSrc: "/nav/wallpapers/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "/products?category=wallpapers&sort=desc",
+        imageSrc: "/nav/wallpapers/new.jpg",
+      },
+      {
+        name: "Bestselling Wallpapers",
+        href: "/products?category=wallpapers",
+        imageSrc: "/nav/wallpapers/bestsellers.jpg",
+      },
+    ],
+  },
+];
